ce0046a9a9c9f9cc92824fb18e4e58368d191dc7
nianxingyan <nianxingyan@oppentech.com>
2024-02-20 18:05:01 +0800

    add pointer in embind

--- demo9/README.md
+++ demo9/README.md
@@ -7,3 +7,5 @@ Demo9导出了一个`Counter`类。
 关注点：
 - 在链接时，需要链接`embind`库
 - 不需要将导出类设置在`EXPORTED_FUNCTIONS`中
+- 通过`embind`导出的函数，无法直接使用指针来传递参数，可以通过`optional_override`来代码函数调用，这样可以不改变原函数的实现
+  - 参考： https://stackoverflow.com/a/27364643
--- demo9/wasm/demo9.cpp
+++ demo9/wasm/demo9.cpp
@@ -20,6 +20,14 @@ public:
     return value;
   }
 
+  int sum(int* numbers, int length) {
+    int v = 0;
+    for (int i=0; i<length; i++) {
+      v += numbers[i];
+    }
+    return v;
+  }
+
 private:
   int max;
   int value;
@@ -34,6 +42,11 @@ EMSCRIPTEN_BINDINGS(my_module) {
     .constructor<int>()
     .function("increase", &Counter::increase)
     .function("getValue", &Counter::getValue)
+    .function("sum", optional_override(
+      [](Counter& self, int ptr, int size) {
+        return self.sum((int*)ptr, size);
+      }
+    ))
     .class_function("getValueFromCounter", &Counter::getValueFromCounter)
     .class_property("version", &Counter::version);
 }
